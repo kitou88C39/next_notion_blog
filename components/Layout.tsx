dd97be7b210d3d29a0ac660577c28b50e20ca3d5
kitou88C39 <kanfutrooper@icloud.com>
2023-02-19 14:28:19 +0900

    Layout.tsx 編集

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,16 +1,17 @@
 import Link from 'next/link';
-import React from 'react';
+import React, { FC, ReactNode } from 'react';
+import { LayoutProps } from '../types/types';
 import Footer from './Footer';
 import Navbar from './Navbar';
 
-const Layout = () => {
+const Layout: FC<LayoutProps> = ({ children }) => {
   return (
     <div className='relative overflow-hidden'>
       <div className='flex flex-col items-center w-full max-w-2xl mx-auto'>
         {/*  */}
         <Navbar />
         {/*  */}
-        <main></main>
+        <main className='w-full px-4 pb-12'>{children}</main>
         {/*  */}
         <Footer />
       </div>
