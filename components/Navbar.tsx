b93c6f6636be3f581dabf31eb48029855161280f
kitou88C39 <kanfutrooper@icloud.com>
2023-02-25 19:31:03 +0900

    types/types.tsを編集

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,10 +10,10 @@ const Navbar = () => {
           className='w-full rounded-md bg-grey-light'
           aria-label='breadcrumb'
         >
-          <Link href='/'>
-            <a className='text-gray-500 hover:text-gray-600'>
-              {siteConfig.title}
-            </a>
+          <Link href='/' className='text-gray-500 hover:text-gray-600'>
+
+            {siteConfig.title}
+
           </Link>
           {/* Breadcrumb */}
         </div>
